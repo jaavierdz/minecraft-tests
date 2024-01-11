9b0923af5055c4c041cc43baa5500a5636f3c797
Javierdz <contact@javitxu.ml>
2024-01-11 23:26:44 +0100

    Create main.java

---
+++ java/giveaway plugin/main.java
@@ -0,0 +1,12 @@
+import org.bukkit.java.plugin
+
+public class App extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Hdf");
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("Seedf!");
+    }
+}
